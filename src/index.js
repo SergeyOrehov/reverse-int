cb5d48d7f522a1d698d0e3b6163cb2b91845227c
SergeyOrehov <34867358+SergeyOrehov@users.noreply.github.com>
2023-01-07 14:24:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse(n) {
     if (n > 0) return +((n + "").split("").reverse().join(""));
-    return +(n + "").slice(1).split("").reverse().join(""));
+    return +(n + "").slice(1).split("").reverse().join("");
 };
